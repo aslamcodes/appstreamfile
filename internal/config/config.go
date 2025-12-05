e1e610be9e25dd2945d28492d998b7d0d864659a
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-05 23:32:36 +0530

    feat: setup catalog configuration implementation

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,9 +3,10 @@ package config
 import "io"
 
 type Config struct {
-	Installers     []Installer    `yaml:"installers"`
-	Files          []File         `yaml:"files"`
-	SessionScripts SessionScripts `yaml:"session_scripts"`
+	Installers     []Installer     `yaml:"installers"`
+	Files          []File          `yaml:"files"`
+	Catalogs        []CatalogConfig `yaml:"catalog"`
+	SessionScripts SessionScripts  `yaml:"session_scripts"`
 }
 
 func (c *Config) Setup(out io.Writer) error {
@@ -27,6 +28,12 @@ func (c *Config) Setup(out io.Writer) error {
 
 	if err := c.SessionScripts.UpdateSessionScriptConfig(out); err != nil {
 		return err
+	}
+
+	for _, catalog := range c.Catalogs {
+		if err := catalog.UpdateStackCatalog(out); err != nil {
+			return err
+		}
 	}
 
 	return nil
