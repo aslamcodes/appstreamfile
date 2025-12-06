ff1e585cc5a2d60598b5f1addd5c48a2ddaa8791
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-06 19:48:01 +0530

    refactor: add context to file Deploy function

--- internal/config/files.go
+++ internal/config/files.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -14,13 +15,13 @@ type File struct {
 
 func (f *File) Deploy(w io.Writer) error {
 	if err := os.MkdirAll(filepath.Dir(f.Path), 0770); err != nil {
-		return err
+		return fmt.Errorf("error creating required directories for %s: %w", f.Path, err)
 	}
 
 	file, err := os.Create(f.Path)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create file on %s: %w", f.Path, err)
 	}
 
 	defer file.Close()
@@ -28,7 +29,7 @@ func (f *File) Deploy(w io.Writer) error {
 	bw := bufio.NewWriter(file)
 
 	if _, err := bw.WriteString(f.Content); err != nil {
-		return err
+		return fmt.Errorf("unable to write to file %s: %w", f.Path, err)
 	}
 
 	return bw.Flush()
