9ad19d74642c7c80f745aaa4e4f669beebde3226
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-07 07:51:58 +0530

    test: add unit test for catalog validator

---
+++ internal/validator/validator.go
@@ -0,0 +1,30 @@
+package validator
+
+import (
+	"errors"
+
+	"github.com/aslamcodes/powerappstream-builder/internal/config"
+)
+
+var (
+	ErrEmptyCatalogName     = errors.New("catalog application name cannot be empty")
+	ErrEmptyCatalogPath     = errors.New("catalog application path cannot be empty")
+	ErrEmptyCatalogNamePath = errors.New("catalog application path and name cannot be empty")
+)
+
+func ValidateCatalogApplications(configData *config.Config) error {
+	for _, c := range configData.Catalogs {
+		if c.Name == "" && c.Path == "" {
+			return ErrEmptyCatalogNamePath
+		}
+		if c.Name == "" {
+			return ErrEmptyCatalogName
+		}
+
+		if c.Path == "" {
+		return ErrEmptyCatalogPath
+		}
+	}
+
+	return nil
+}
