667a96a9c5fafc1d7c092a09026b85bd03f434a8
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-07 10:13:12 +0530

    feat: add validation for platform

---
+++ internal/validator/config_validator.go
@@ -0,0 +1,32 @@
+package validator
+
+import (
+	"errors"
+	"slices"
+
+	"github.com/aslamcodes/powerappstream-builder/internal/config"
+)
+
+var (
+	ErrPlatformMissing = errors.New("config file does not contain platform specification")
+	ErrInvalidPlatform = errors.New("Platform not supported")
+)
+
+
+func ValidateConfig(c *config.Config) error {
+	if c.Platform == "" {
+		return ErrPlatformMissing
+	}
+
+	supportedPlatforms := make([]string, len(ExecPlatformMap))
+
+	for k := range ExecPlatformMap {
+		supportedPlatforms = append(supportedPlatforms, k)
+	}
+
+	if !slices.Contains(supportedPlatforms, c.Platform) {
+		return ErrInvalidPlatform
+	}
+
+	return nil
+}
