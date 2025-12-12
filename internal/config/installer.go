395b4e5bd0fd42a640822bd7bcde5daca1164425
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-12 22:53:52 +0530

    refactor: refactor installer to unified of platforms

--- internal/config/installer.go
+++ internal/config/installer.go
@@ -1,10 +1,55 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 type Installer struct {
 	InstallScript string `yaml:"installScript"`
 	Executable    string `yaml:"executable"`
 }
 
-func (i *Installer) Install() error {
-	return PlatformInstaller(i)
+func (inst *Installer) Install() error {
+	switch inst.Executable {
+	case "powershell":
+		return runScript("powershell.exe", ".ps1", inst.InstallScript, "-NoProfile", "-NonInteractive", "-ExecutionPolicy", "Bypass", "-File")
+	case "bash":
+		// return runScript("bash", ".sh", inst.InstallScript)
+	default:
+		return fmt.Errorf("unsupported executable: %s", inst.Executable)
+	}
+	return nil
+}
+
+func runScript(exe, ext, body string, argsPrefix ...string) error {
+	f, err := os.CreateTemp(os.TempDir(), "installer-*"+ext)
+
+	if err != nil {
+		return fmt.Errorf("unable to create temporary file: %w", err)
+	}
+
+	defer func() {
+		f.Close()
+		os.Remove(f.Name())
+	}()
+
+	if _, err := f.Write([]byte(body)); err != nil {
+		return fmt.Errorf("error writing to file %s: %w", f.Name(), err)
+	}
+
+	args := append(argsPrefix, f.Name())
+
+	cmd := exec.Command(exe, args...)
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return fmt.Errorf("error executing command %s: %w", cmd.String(), err)
+	}
+
+	fmt.Println(string(output))
+
+	return nil
 }
